c460513b894c7385f9451f4e4da462434f0d9fd6
jmn <utc002@gmail.com>
2020-07-25 10:09:57 +0300

    initial commit

---
+++ src/main/java/com/jmn/helloworld/HelloController.java
@@ -0,0 +1,20 @@
+package com.jmn.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: jmn
+ * @Date: 24.07.2020
+ **/
+
+@RestController
+public class HelloController {
+
+    public HelloController() {}
+
+    @RequestMapping(value = "/hello")
+    public String helloEndPoint() {
+        return "hello Eugene!";
+    }
+}
